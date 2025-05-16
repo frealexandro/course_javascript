739e2a94c67c6576a27aaddee785b7635a28cca1
frealexandro <jhoner300@gmail.com>
2025-05-16 16:15:51 -0500

    i create a object

---
+++ 01-tipos/06-objetos.js
@@ -0,0 +1,11 @@
+//Personaje de netflix
+let nombre = "Tanjiro";
+let anime = "Demon slayer";
+let edad = 16;
+
+let personaje = {
+    nombre : "Tanjiro",
+    anime : "Demon slayer" ,
+    edad : 16 , 
+};
+console.log(personaje);
