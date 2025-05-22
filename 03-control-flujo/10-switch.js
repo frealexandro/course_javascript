1d8654e3fcb4dc0a8046de46e23141e5835bd107
frealexandro <jhoner300@gmail.com>
2025-05-22 12:09:31 -0500

    i create the fist excersice

---
+++ 03-control-flujo/10-switch.js
@@ -0,0 +1,15 @@
+let accion = 'actualizar';
+
+switch(accion){
+
+    case 'listar':
+        console.log('accion de listar');
+        break;
+    case 'gaurdar':
+        console.log('accion de guardar');
+        break;
+    default:
+        console.log('accion no reconocida')
+
+
+}
---
+++ 04-ejercicios/01.js
@@ -0,0 +1,31 @@
+
+
+//primer metodo funcion normal
+
+// function cualEsMayor(a , b) {
+//     if (a > b ){
+
+//     return a;
+
+//     }else{
+
+//     return b;
+    
+//     }
+
+
+// }
+
+
+//segundo metodo operador ternario
+
+function cualEsMayor(a,b){
+    return ( a > b ) ? a : b;
+}
+
+let mayor = cualEsMayor(10 , 25);
+
+console.log(mayor);
+
+
+
